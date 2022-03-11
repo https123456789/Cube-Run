6aceb36f55043cc6259fc2da655343ccbbc0a1db
Ben Landon <81256789+https123456789@users.noreply.github.com>
2022-03-11 18:35:45 +0000

    Added help menu

--- res/js/main.js
+++ res/js/main.js
@@ -2,6 +2,7 @@ var game = new Game();
 
 // Connect "buttons" on the home screen to the game
 var startButton = document.getElementById("startButton");
+var helpButton = document.getElementById("helpButton");
 var creditsButton = document.getElementById("creditsButton");
 var loadingDiv = document.getElementById("loadingDiv");
 
@@ -18,12 +19,26 @@ startButton.onclick = () => {
 	//document.getElementById("startMenu").classList.add("fade");
 	startButton.classList.add("fade");
 	creditsButton.classList.add("fade");
+	helpButton.classList.add("fade");
 	loadingDiv.classList.add("fade-in");
 	//startButton.classList.add("spin-dont-save");
 };
 
+helpButton.onclick = () => {
+	var el = document.createElement("iframe");
+	el.src = "help.html";
+	el.id = "helpIframe";
+	el.classList.add("iframeWindow");
+	//el.style.display = "none";
+	document.body.appendChild(el);
+	el.onload = () => {
+		//el.style.display = "block";
+		el.classList.add("slide-up");
+	}
+}
+
 creditsButton.onclick = () => {
-	creditsButton.classList.add("spin-dont-save");
+	//creditsButton.classList.add("spin-dont-save");
 	rollCredits();
 };
 
@@ -31,6 +46,7 @@ function rollCredits() {
 	var el = document.createElement("iframe");
 	el.src = "credits.html";
 	el.id = "creditsIframe";
+	el.classList.add("iframeWindow");
 	el.style.display = "none";
 	document.body.appendChild(el);
 	el.onload = () => {
@@ -40,13 +56,19 @@ function rollCredits() {
 }
 
 window.onmessage = (event) => {
-	if (event.data == "creditsEnded") {
-		window.setTimeout(() => {
-			document.body.removeChild(
-				document.getElementById("creditsIframe")
-			);
-		}, 1000);
-		//document.getElementById("creditsIframe").classList.remove("slide-up");
-		//document.getElementById("creditsIframe").classList.add("slide-off-screen");
+	switch (event.data) {
+		case "creditsEnded":
+			window.setTimeout(() => {
+				document.body.removeChild(
+					document.getElementById("creditsIframe")
+				);
+			}, 1000);
+			break;
+		case "helpEnded":
+			window.setTimeout(() => {
+				document.body.removeChild(
+					document.getElementById("helpIframe")
+				);
+			}, 1000);
 	}
 }
